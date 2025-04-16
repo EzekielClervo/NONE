c38b4a3a5365ae6e3bbf8e1567346a11f538198d
EzekielClervo <150000384+EzekielClervo@users.noreply.github.com>
2025-04-17 02:30:14 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "facebook-automation-suite",
+  "version": "1.0.0",
+  "description": "Facebook Automation Suite using Express with automation endpoints and SQLite token storage",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "axios": "^1.3.4",
+    "chalk": "^4.1.2",
+    "express": "^4.18.2",
+    "ora": "^6.0.1",
+    "sqlite3": "^5.1.6",
+    "uuid": "^9.0.0",
+    "body-parser": "^1.20.2"
+  }
+}
